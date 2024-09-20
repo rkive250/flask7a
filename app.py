fa3d9fd0a264c63e75da6fdd8bee4c685e841957
rkive250 <rkive250@gmail.com>
2024-09-19 20:14:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,52 @@
-from flask import Flask
-
-from flask import render_template
-from flask import request
-
+from flask import Flask, render_template, request
 import pusher
-
 import mysql.connector
 import datetime
 import pytz
 
-con = mysql.connector.connect(
-  host="185.232.14.52",
-  database="u760464709_tst_sep",
-  user="u760464709_tst_sep_usr",
-  password="dJ0CIAFF="
-)
-
 app = Flask(__name__)
+
+def get_db_connection():
+    con = mysql.connector.connect(
+        host="185.232.14.52",
+        database="u760464709_tst_sep",
+        user="u760464709_tst_sep_usr",
+        password="dJ0CIAFF="
+    )
+    return con
 
 @app.route("/")
 def index():
-    con.close()
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
-    con.close()
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    con.close()
-    telefono= request.form["tel"]
-    nombre_curso= request.form["ncurso"]
-
-    return f"telefono {telefono} telefono: {telefono}"
+    telefono = request.form["tel"]
+    nombre_curso = request.form["ncurso"]
+    return f"telefono {telefono} curso: {nombre_curso}"
 
 @app.route("/buscar")
 def buscar():
+    con = get_db_connection()
     if not con.is_connected():
         con.reconnect()
 
     cursor = con.cursor()
     cursor.execute("SELECT * FROM tst0_cursos")
-    
     registros = cursor.fetchall()
 
-    return registros;
+    cursor.close()
+    con.close()
+
+    return str(registros)
 
 @app.route("/evento", methods=["GET"])
 def evento():
-   args = request.args
-   pusher_client = pusher.Pusher(
+    pusher_client = pusher.Pusher(
         app_id='1864232',
         key='ec020425c2206acb32eb',
         secret='a5091fe74dbda031cda4',
@@ -59,22 +54,21 @@ def evento():
         ssl=True
     )
 
+    con = get_db_connection()
+
     if not con.is_connected():
-    
         con.reconnect()
 
     cursor = con.cursor()
-
     args = request.args
-  
-    sql = "INSERT INTO tst0_cursos (Telefono , Nombre_Curso)) VALUES (%s,%s)"
-    val = (args["tel"],args["ncurso"] )
 
+    sql = "INSERT INTO tst0_cursos (Telefono, Nombre_Curso) VALUES (%s, %s)"
+    val = (args["tel"], args["ncurso"])
     cursor.execute(sql, val)
-    
     con.commit()
+
+    cursor.close()
     con.close()
 
-   
-    pusher_client.trigger("conexion", "evento", request.args)
+    pusher_client.trigger("conexion", "evento", args)
     return args
