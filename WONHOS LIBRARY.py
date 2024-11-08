de89b23205614e423dccef2b52e5cca8a8a383ed
rkive250 <rkive250@gmail.com>
2024-11-07 19:02:15 -0600

    Update WONHOS LIBRARY.py

--- WONHOS LIBRARY.py
+++ WONHOS LIBRARY.py
@@ -5,37 +5,37 @@ app = Flask(__name__)
 # Ruta principal
 @app.route('/')
 def home():
-    return render_template('app.html')
+    return render_template('WL.html')
 
 @app.route('/reserva', methods=['GET', 'POST'])
 def reserva():
     if request.method == 'POST':
         return "Reserva realizada."
-    return render_template('app.html')
+    return render_template('WL.html')
 
 @app.route('/buscar', methods=['GET', 'POST'])
 def buscar():
     if request.method == 'POST':
         return "Búsqueda realizada."
-    return render_template('app.html')
+    return render_template('WL.html')
 
 @app.route('/categorias')
 def categorias():
-    return render_template('app.html')
+    return render_template('WL.html')
 
 @app.route('/ocupados')
 def ocupados():
-    return render_template('app.html')
+    return render_template('WL.html')
 
 @app.route('/inicio-sesion', methods=['GET', 'POST'])
 def inicio_sesion():
     if request.method == 'POST':
         return "Inicio de sesión realizado."
-    return render_template('app.html')
+    return render_template('WL.html')
 
 @app.route('/proximos-stock')
 def proximos_stock():
-    return render_template('app.html')
+    return render_template('WL.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
