340b36c9f925ab4be4c67a6ae1bb259c8d3191e5
rkive250 <rkive250@gmail.com>
2025-09-09 01:17:55 -0500

    Update sw.js

--- REFUGIO/sw.js
+++ REFUGIO/sw.js
@@ -3,18 +3,15 @@ const archivos = [
   "/",
   "/index.html",
   "/styles.css",
-  "/app.js",
-  "/libros.json"
+  "/app.js"
 ];
 
-// Instalar Service Worker
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(archivos))
   );
 });
 
-// Interceptar solicitudes
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
