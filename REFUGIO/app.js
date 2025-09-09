831a774eaa916c4e0a63cfc9f652d946ebfaee77
rkive250 <rkive250@gmail.com>
2025-09-09 01:55:21 -0500

    Delete REFUGIO directory

--- REFUGIO/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(() => console.log("✅ Service Worker registrado"))
-    .catch(err => console.error("❌ Error registrando SW:", err));
-}
